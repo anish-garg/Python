503ded1a960941d0fbebcdaa9c92003d1828416f
anish-garg <anishgarg5914@gmail.com>
2023-11-24 04:41:29 +0530

    Day_6 completed

--- Day_6.py
+++ Day_6.py
@@ -10,5 +10,7 @@ number = 6
 while number > 0:
     number -= 1
     print(number)
+    
+# Practiced of both functions and while loop at reeborgs World site -  https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
     
