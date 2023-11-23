90a7683b3a8e3bfe8cf52a66dd1ad6317e1cf600
anish-garg <anishgarg5914@gmail.com>
2023-11-24 04:36:45 +0530

    Day_6 completed

---
+++ Day_6.py
@@ -0,0 +1,14 @@
+# Functions
+def my_function():
+    print("Hello")
+    print("World")
+    
+my_function()      
+
+# While loop
+number = 6
+while number > 0:
+    number -= 1
+    print(number)
+
+    
